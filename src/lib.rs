783d3ace1ae2a30475fee0e91f74f59f73002c6b
Arbaz <arbazk1444@gmail.com>
2023-04-19 21:49:31 +0530

    implement fetch function

--- src/lib.rs
+++ src/lib.rs
@@ -70,6 +70,14 @@ impl Emu {
         self.st = 0;
         self.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
     }
+
+    fn fetch(&mut self) -> u16 {
+        let higher_byte = self.ram[self.pc as usize] as u16;
+        let lower_byte = self.ram[(self.pc + 1) as usize] as u16;
+        let op = (higher_byte << 8) | lower_byte;
+        self.pc += 2;
+        op
+    }
 }
 
 impl Emu {
